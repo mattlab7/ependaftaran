af1a64401e139bc8f38bb1b2228ea98bafa27d29
Matt <43501106+dotcatfish@users.noreply.github.com>
2019-05-29 01:33:46 +0800

    Update index.php

--- info/index.php
+++ info/index.php
@@ -41,7 +41,7 @@
 		<h1><a href="../">e-Pendaftaran</a></h1>
 		<h3>SMK Sultan Abdul Samad</h3>
 		<h4>Info</h4>
-		<p>Pangkalan data ini ditubuhkan untuk Kerja Kursus SPM Sains Komputer, 2018 </br> [redacted for archival]</p>
+		<p>Pangkalan data ini ditubuhkan untuk Kerja Kursus SPM Sains Komputer, 2018 </br> [redacted]</p>
 		<h4>Penghargaan</h4>
 		<table>
 			<tr>
